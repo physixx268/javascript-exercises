a6112fec42c1a068304efb4904d1bed2d041cc01
physixx268 <53198392+physixx268@users.noreply.github.com>
2022-12-12 05:23:17 +0100

    02 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, repetitions) {
+    let stringRepeated = "";
+
+    if (Math.sign(repetitions) == 1) {
+
+        for (i = 1; i <= repetitions; i++) {
+            stringRepeated = stringRepeated + string;
+        }
+
+        return stringRepeated;
+    } else if (Math.sign(repetitions) == 0) {
+
+        return "";
+
+    } else {
+
+        return "ERROR";
+    }
 
 };
 
