9359b8100d098e2d3100ee0c17b5ae00892c8cb6
physixx268 <53198392+physixx268@users.noreply.github.com>
2022-12-13 18:48:22 +0100

    04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+ /*   const array = args[0];
 
-};
+    const newArray = [];
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!args.includes(item)) {
+          newArray.push(item);
+        }
+      });
+      // and return that array
+      return newArray;
 
+      */
+
+   const array = args[0]
+   return array.filter(val => !args.includes(val))
+    };
 // Do not edit below this line
 module.exports = removeFromArray;
