bbceb86df23b3a8c870e84f9f3ba758429412f7a
physixx268 <53198392+physixx268@users.noreply.github.com>
2022-12-14 14:03:54 +0100

    temp Conversion passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fTemp = fahrenheit;
+  let fToCel = (fTemp - 32) * 5 / 9;
+  let rounded = Math.round(fToCel * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let cTemp = celsius;
+  let cToFahr = cTemp * 9 / 5 + 32;
+  let rounded = Math.round(cToFahr * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
