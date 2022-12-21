1204055fc1d7ba51f4cbbc9c7ac882168bb02259
physixx268 <53198392+physixx268@users.noreply.github.com>
2022-12-21 17:03:29 +0100

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (sum1, sum2) {
+  let res = sum1 + sum2;
+
+  return res;
 };
 
-const subtract = function() {
-	
+const subtract = function (sum1, sum2) {
+  let res = sum1 - sum2;
+
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const res = arr.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0);
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const res = arr.reduce((accumulator, value) => {
+    return accumulator * value;
+  }, 1);
+  return res;
 };
 
-const power = function() {
-	
+const power = function (sum1, sum2) {
+  let res = Math.pow(sum1, sum2);
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
